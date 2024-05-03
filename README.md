7d8bbfe7fe2b7c8fd61806ef0f5205b25099bead
KronosDP <darrel.danadyaksa19@gmail.com>
2024-05-03 11:43:25 +0700

    Experiment 1.3: Multiple Spawn and removing drop

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ![alt text](ss1.png)
 
 Notice that hey hey came first. Notice. We can easily understand this because `executor.run()` prints out our howdy and done. Notice that above executor.run exists hey hey. That is why we print hey hey first, then howdy and done. ALso, notice that the method that encapsulates howdy and done have async.
+
+# Multiple Spawn and removing drop
+
+![alt text](ss2.png)
+
+Remember `async`? This is what's happening. Notice that after howdy, we are using timer to wait. In the meantime, other task are being run. Here's what's happening: howdy is first being printed, then it waits to print done. Notice that howdy2 and howdy 3 is `async` right? So we print (do that job) that too and the done, done2, and done3 waits until the timer runs out.
--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,20 @@ fn main() {
         println!("KronosDP's computer: done!");
     });
 
+    spawner.spawn(async {
+        println!("KronosDP's computer: howdy2!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("KronosDP's computer: done2!");
+    });
+
+    spawner.spawn(async {
+        println!("KronosDP's computer: howdy3!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("KronosDP's computer: done3!");
+    });
+
     println!("KronosDP's computer: hey hey");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
